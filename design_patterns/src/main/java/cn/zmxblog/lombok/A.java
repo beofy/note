41c20a1c5da15d6b37b1ac97f6ab400637b3a96c
minxiang <294315568@qq.com>
2019-01-26 11:06:44 +0800

    lombok插件

---
+++ design_patterns/src/main/java/cn/zmxblog/lombok/A.java
@@ -0,0 +1,17 @@
+package cn.zmxblog.lombok;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2019/1/22 22:20:20
+ */
+@Data
+public class A {
+
+    private String name;
+    private String password;
+
+}
---
+++ design_patterns/src/main/java/cn/zmxblog/lombok/Main.java
@@ -0,0 +1,13 @@
+package cn.zmxblog.lombok;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2019/1/22 22:22:22
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(a.toString());
+    }
+}
